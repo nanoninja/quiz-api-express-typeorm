1629bf273d29a6c7a511e4a9af10554ddd872576
Vincent Letourneau <vincent@nanoninja.com>
2019-12-26 17:17:46 +0100

    Improve code with remove promise by await

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -15,7 +15,9 @@ function getRepository(): UserRepository {
 /**
  * Creates a new user instance.
  */
-export async function createUser(request: Request, response: Response) {
+export async function createUser(request: Request, response: Response): Promise<void> {
+
+    // get a user repository to perform operations with user.
     const repo: UserRepository = getRepository();
     const data = request.body;
     const user = new User();
@@ -27,143 +29,141 @@ export async function createUser(request: Request, response: Response) {
 
     if (errors.length > 0) {
         response.status(422).json({ message: 'Unprocessable Entity', errors: errors });
-    } else {
-        await repo.save(user)
-            .then(
-                (user: User) => response.json(user),
-                (err: any) => {
-                    if (err.code && err.code === 'ER_DUP_ENTRY') {
-                        response.status(422).json({ message: 'Unprocessable Entity' });
-                        return;
-                    }
-                    response.status(500).json('Internal Server Error');
-                }
-            );
+        return;
     }
+
+    repo.save(user)
+        .then(
+            (user: User) => response.json(user),
+            (err: any) => {
+                if (err.code && err.code === 'ER_DUP_ENTRY') {
+                    response.status(422).json({ message: 'Unprocessable Entity' });
+                    return;
+                }
+                response.status(500).json('Internal Server Error');
+            }
+        );
 }
 
 /**
  * Gets all users.
  */
 export async function getUsers(request: Request, response: Response) {
+
+    // Get a user repository to perform operations with user.
     const repo: UserRepository = getRepository();
 
-    await repo.find()
-        .then(
-            (users: User[]) => response.json(users),
-            (err: Error) => response.status(500).json({ message: 'Internal Server Error' })
-        );
+    // Load a users.
+    const users: User[] = await repo.find();
+
+    // Send saved status back.
+    response.json(users);
 }
 
 /**
  * Gets an user by its id.
  */
-export async function getUserById(request: Request, response: Response) {
-    const repo: UserRepository = getRepository();
-    const id = request.params.id;
+export async function getUserById(request: Request, response: Response): Promise<void> {
 
-    await repo.findById(id)
-        .then(
-            (user: User | undefined) => {
-                if (!user) {
-                    response.status(404).json({ message: 'Not Found' });
-                    return;
-                }
-                response.json(user);
-            },
-            (err: Error) => {
-                response.status(500).json({ message: 'Internal Server Error' })
-            }
-        );
+    // Get a user repository to perform operations with user.
+    const repo: UserRepository = getRepository();
+
+    // Load a user by a given user id.
+    const user: User | undefined = await repo.findById(request.params.id);
+
+    if (!user) {
+        response.status(404).end();
+        return;
+    }
+
+    // Send loaded user.
+    response.json(user);
 }
 
 /**
  * Gets an user by its email.
  */
-export async function getUserByEmail(request: Request, response: Response) {
-    const repo: UserRepository = getRepository();
-    const email = request.params.email;
+export async function getUserByEmail(request: Request, response: Response): Promise<void> {
 
-    if (!email) {
-        response.status(400).json({ message: 'Bad Request' });
+    // get a user repository to perform operations with user.
+    const repo: UserRepository = getRepository();
+
+    // Load a user by a given user email.
+    const user: User | undefined = await repo.findByEmail(request.params.email);
+
+    if (!user) {
+        response.status(404).end();
+        return;
     }
 
-    await repo.findByEmail(email)
-        .then(
-            (user: User | undefined) => {
-                if (!user) {
-                    response.status(404).json({ message: 'Not Found' });
-                    return;
-                }
-                response.json(user);
-            },
-            (err: Error) => response.status(500).json({ message: 'Internal Server Error' })
-        );
+    // Send loaded user.
+    response.json(user);
 }
 
 /**
  * Removes an user by its id.
  */
-export async function removeUser(request: Request, response: Response) {
-    const repo: UserRepository = getRepository();
-    const id = request.params.id;
+export async function removeUser(request: Request, response: Response): Promise<void> {
 
-    if (!id) {
-        response.status(400).json({ message: 'Bad Request' });
+    // get a user repository to perform operations with user.
+    const repo: UserRepository = getRepository();
+
+    // Load a user by a given user id.
+    const user: User | undefined = await repo.findById(request.params.id);
+
+    if (!user) {
+        response.status(404).end();
+        return;
     }
 
-    await repo.findById(id)
-        .then(
-            (user: User | undefined) => {
-                if (!user) {
-                    response.status(404).json({ message: 'Not Found' });
-                    return;
-                }
-                repo.remove(user)
-                    .then(
-                        (user: User) => response.status(204).json({}),
-                        (err: Error) => response.status(500).json({ message: 'Internal Server Error' })
-                    );
-            },
-            (err: Error) => response.status(500).json({ message: 'Internal Server Error' })
-        );
-
+    try {
+        await repo.remove(user);
+        response.status(204).end();
+    } catch (err) {
+        response.status(500).end();
+    }
 }
 
 /**
  * Updates a user entity.
  */
-export async function updateUser(request: Request, response: Response) {
+export async function updateUser(request: Request, response: Response): Promise<void> {
+
+    // Get a user repository to perform operations with user.
     const repo: UserRepository = getRepository();
     const body = request.body;
-    const id = body.id;
-    const user = await repo.findById(id);
+
+    if (!hasObjectProperties(body, ['id', 'email', 'firstName', 'lastName'])) {
+        response.status(400).json('Bad Request');
+        return;
+    }
+
+    // Load a user by a given id.
+    let user: User | undefined = await repo.findById(body.id);
 
     if (!user) {
-        response.status(422).json({ message: 'Unprocessable Entity' });
-    } else {
-        if (!hasObjectProperties(body, ['id', 'firstName', 'lastName'])) {
-            response.status(400).json('Bad Request');
-        } else {
-            user.id = body.id;
-            user.email = body.email;
-            user.firstName = body.firstName;
-            user.lastName = body.lastName;
-            user.isActive = body.isActive;
-
-            const errors = await validate(user);
-
-            if (errors.length > 0) {
-                response.status(422).json({ message: 'Unprocessable Entity' });
-            } else {
-                repo.save(user)
-                    .then(
-                        (user: User) => response.status(204).json({}),
-                        (err: any) => response.status(422).json({ message: 'Unprocessable Entity' })
-                    );
-            }
-        }
+        response.status(404).json({ message: 'Unprocessable Entity' });
+        return;
     }
+
+    user.id = body.id;
+    user.email = body.email;
+    user.firstName = body.firstName;
+    user.lastName = body.lastName;
+    user.isActive = body.isActive;
+
+    const errors = await validate(user);
+
+    if (errors.length > 0) {
+        response.status(422).json({ message: 'Unprocessable Entity' });
+        return;
+    }
+
+    // Save received user.
+    await repo.save(user);
+
+    // Send saved status back.
+    response.status(204);
 }
 
 function hasObjectProperties(o: any, properties: string[]): boolean {
